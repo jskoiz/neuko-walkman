5574a15c19ea3fb87fb75f8b14d59ab09d33f039
Jerry <jerry@avmillabs.com>
2025-11-17 13:27:30 -1000

    Add Vercel adapter for serverless API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [react()]
 });
